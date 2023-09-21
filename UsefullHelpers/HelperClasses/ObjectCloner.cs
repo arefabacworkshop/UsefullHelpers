b4ec21697975ddd55b5e4b15d80dd4475a9d9ae2
a.abbasi <a.abbasi@sjicompany.com>
2023-09-21 10:04:41 +0330

    ObjectCloner Class

---
+++ UsefullHelpers/HelperClasses/ObjectCloner.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsefullHelpers.HelperClasses
+{
+    public static class ObjectCloner
+    {
+        public static T CloneObject<T>(T obj)
+        {
+            if (obj == null)
+                return default(T);
+
+            string serializedObject = JsonConvert.SerializeObject(obj);
+            return JsonConvert.DeserializeObject<T>(serializedObject);
+        }
+    }
+
+}
